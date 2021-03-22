b637d0a270a831884bb64cb9ed1c5d8ed0c1767e
Hongcheol Kim <hongcheolkim@Hongcheolui-MacBookPro.local>
2021-03-22 20:32:58 +0900

    github daily practice

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import A, { componentB, word } from './index.copy';
 
 const asd = '';
 console.log(word);
+document.onload = function(){
+    alert("Hello World!!");
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports={
     mode: 'production', // or "development" or "none"
-    entry: './src/index.js',
-
+    entry: './src/server.js',
+    target: 'node',
     output: {
         filename: '[name]_bundle.js',
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'dist'),
       },
     resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.json', '.jsx', '.css'],
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.css'],
     },
 };
